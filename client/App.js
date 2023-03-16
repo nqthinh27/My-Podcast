f2e5d3dfed4e3959b056b7d0feabb2279fad492d
NQT <thinhnb2712@gmail.com>
2023-03-17 02:37:29 +0700

    modify status bar and init redux

--- client/App.js
+++ client/App.js
@@ -1,9 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Navigator from './src/navigate/Navigator';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-      <Navigator/>
+    <Provider store={store}>
+      <StatusBar style="dark"></StatusBar>
+      <Navigator />
+    </Provider>
   );
 }
---
+++ client/src/redux/authSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        login: {
+            currentUser: null,
+            isFetching: false,
+            error: false,
+        },
+    },
+    reducers: {
+        loginStart: (state) => {
+            state.login.isFetching = true;
+        },
+        loginSuccess: (state, action) => {
+            state.login.isFetching = false;
+            state.login.currentUser = action.payload;
+            state.login.error = false;
+        },
+        loginFailed: (state) => {
+            state.login.isFetching = false;
+            state.login.error = true;
+        },
+        logout: (state) => {
+            state.currentUser = null;
+        },
+    }
+});
+
+export const { loginStart, loginSuccess, loginFailed, logout } = authSlice.actions;
+
+export default authSlice.reducer;
