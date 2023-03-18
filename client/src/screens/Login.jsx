9a3bb0f135885068b03c7fac5a33fb8002259e24
binota17 <vantambui4@gmail.com>
2023-03-19 00:00:10 +0700

    update login, register, forgot password

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,229 +1,244 @@
-import { React, useState } from 'react'
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native'
-import Icon from 'react-native-vector-icons/Entypo'
-import colors from '../constrants/colors'
+import { React, useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    SafeAreaView,
+} from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import colors from "../constants/colors";
 
 function Login(props) {
-  //navigation
-  // const { navigation, route } = props;
-  //  //function of navigate 
-  // const { navigate, goback } = navigation;
+    //navigation
+    // const { navigation, route } = props;
+    // //function of navigate
+    // const { navigate, goback } = navigation;
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loginwGoogle, setLoginwGoogle] = useState(false)
-  const [loginwFb, setLoginwFb] = useState(false)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginwGoogle, setloginwGoogle] = useState(false);
+    const [loginwFb, setloginwFb] = useState(false);
 
-  return (
-    <SafeAreaView style={styles.login}>
+    return (
+        <SafeAreaView style={styles.login}>
+            <View style={styles.loginHeader}>
+                <Icon
+                    name={"chevron-left"}
+                    size={30}
+                    // color={colors.primary}
+                    onPress={() => {
+                        navigate("UIScreen");
+                    }}
+                />
+                <Text style={styles.loginTextHeader}>Đăng nhập</Text>
+                <Text> </Text>
+            </View>
 
-      <View style={styles.loginHeader}>
-        <Icon style={styles.back}
-          name={'chevron-left'}
-          size={26}
-          // color={colors.primary}
-          onPress={() => { navigate('UIScreen') }}
-        />
-        <Text style={styles.loginTextHeader}>Đăng nhập</Text>
-        <Text> </Text>
-      </View>
+            <View style={styles.loginContainer}>
+                <View style={styles.loginInputEmail}>
+                    <TextInput
+                        style={styles.loginTextInputEmail}
+                        value={email}
+                        onChangeText={setEmail}
+                        placeholder="Email"
+                    ></TextInput>
+                </View>
 
-      <View style={styles.LoginContainer}>
-        <View style={styles.LoginInputEmail}>
-          <TextInput style={styles.LoginTextInputEmail}
-            value={email}
-            onChangeText={setEmail}
-            placeholder='Email'
-          ></TextInput>
-        </View>
+                <View style={styles.loginInputPassword}>
+                    <TextInput
+                        style={styles.loginTextInputPassword}
+                        value={password}
+                        onChangeText={setPassword}
+                        placeholder="Mật khẩu"
+                        secureTextEntry={true}
+                    ></TextInput>
 
-        <View style={styles.LoginInputPassword}>
-          <TextInput style={styles.LoginTextInputPassword}
-            value={password}
-            onChangeText={setPassword}
-            placeholder='Mật khẩu'
-            secureTextEntry={true}
-          ></TextInput>
+                    <Text style={styles.loginTextForgot}>Quên mật khẩu?</Text>
+                </View>
 
-          <Text
-            style={styles.LoginTextForgot}
-          >Quên mật khẩu?</Text>
-        </View>
+                <TouchableOpacity
+                    style={styles.loginButtonViewlogin}
+                    onPress={() => {
+                        variable.islogin = 1;
+                    }}
+                >
+                    <Icon name="login" size={25} color="#fff" />
+                    <Text style={styles.loginButtonlogin}> Đăng nhập</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.LoginButtonViewLogin}
-          onPress={() => {
-            variable.isLogin = 1;
-          }} >
-          <Icon name="login" size={25} color="#fff" />
-          <Text style={styles.LoginButtonLogin}>  Đăng nhập</Text>
-        </TouchableOpacity>
+                <TouchableOpacity style={styles.loginButtonViewGoogle}>
+                    <Icon
+                        name="google--with-circle"
+                        size={35}
+                        color="#ED5A4F"
+                    />
+                    <Text style={styles.loginButtonGoogle}>
+                        {" "}
+                        Đăng nhập với Google
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.loginButtonViewFb}>
+                    <Icon name="facebook" size={33} color="#0571E6" />
+                    <Text style={styles.loginButtonFb}>
+                        {" "}
+                        Đăng nhập với Facebook
+                    </Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity style={styles.LoginButtonViewGoogle} >
-          <Icon name="google--with-circle" size={35} color="#ED5A4F" />
-          <Text style={styles.LoginButtonGoogle}>  Đăng nhập với Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.LoginButtonViewFb}>
-          <Icon name="facebook" size={33} color="#0571E6" />
-          <Text style={styles.LoginButtonFb} >  Đăng nhập với Facebook</Text>
-        </TouchableOpacity>
-
-        <View style={styles.LoginViewNoEmail}>
-          <Text style={styles.LoginTextNoEmail}>Chưa có tài khoản? </Text>
-          <Text
-            style={styles.LoginButtonNoEmail}
-            onPress={() => {
-              navigate('Register');
-            }}>Đăng kí
-          </Text>
-        </View>
-
-      </View>
-
-    </SafeAreaView>
-  )
+                <View style={styles.loginViewNoEmail}>
+                    <Text style={styles.loginTextNoEmail}>
+                        Chưa có tài khoản?{" "}
+                    </Text>
+                    <Text
+                        style={styles.loginButtonNoEmail}
+                        onPress={() => {
+                            navigate("Register");
+                        }}
+                    >
+                        Đăng kí
+                    </Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  login: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
+    login: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
 
-  loginHeader: {
-    flex: 1,
-    marginHorizontal: 20,
-    marginTop: 40,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start"
-  },
+    loginHeader: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+    },
 
-  loginTextHeader: {
-    fontWeight: "bold",
-    fontSize: 21
-  },
+    loginTextHeader: {
+        fontWeight: "bold",
+        fontSize: 21,
+    },
 
-  LoginContainer: {
-    flex: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20
-  },
+    loginContainer: {
+        flex: 20,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        marginHorizontal: 16,
+    },
 
-  LoginInputEmail: {
-    marginHorizontal: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#C0C0C0',
-    width: '90%',
-  },
+    loginInputEmail: {
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: "#C0C0C0",
+        width: "100%",
+    },
 
-  LoginTextInputEmail: {
-    fontSize: 15
-  },
+    loginTextInputEmail: {
+        fontSize: 15,
+    },
 
-  LoginInputPassword: {
-    flexDirection: "row",
-    marginHorizontal: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#C0C0C0',
-    width: '90%',
-    marginTop: 46,
-    justifyContent: "space-between"
-  },
+    loginInputPassword: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: "#C0C0C0",
+        width: "100%",
+        marginTop: 46,
+        justifyContent: "space-between",
+    },
 
-  LoginTextForgot: {
-    color: "#FF6363",
-    fontSize: 15
-  },
+    loginTextForgot: {
+        color: "#FF6363",
+        fontSize: 15,
+    },
 
-  LoginViewNoEmail: {
-    flexDirection: "row",
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 63,
+    loginViewNoEmail: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 63,
+    },
+    loginTextInputPassword: {
+        flex: 1,
+        fontSize: 15,
+    },
+    loginTextNoEmail: {
+        color: "#B0ADAD",
+        fontSize: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: "#FF6363",
+    },
 
-  },
-  LoginTextInputPassword: {
-    flex: 1,
-    fontSize: 15
+    loginButtonNoEmail: {
+        fontSize: 15,
+        color: "#FF6363",
+        textDecorationline: "underline",
+        textDecorationStyle: "solid",
+        textDecorationColor: "#FF6363",
+    },
 
-  },
-  LoginTextNoEmail: {
-    color: '#B0ADAD',
-    fontSize: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#FF6363'
-  },
+    loginButtonViewlogin: {
+        width: "100%",
+        height: 55,
+        borderRadius: 25,
+        borderWidth: 1,
+        backgroundColor: colors.primary,
+        alignItems: "center",
+        justifyContent: "center",
+        borderColor: "white",
+        marginTop: 63,
+        flexDirection: "row",
+    },
+    loginButtonlogin: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 17,
+        alignItems: "center",
+        justifyContent: "center",
+    },
 
-  LoginButtonNoEmail: {
-    fontSize: 15,
-    color: "#FF6363",
-    textDecorationLine: "underline",
-    textDecorationStyle: "solid",
-    textDecorationColor: "#FF6363",
-  },
+    loginButtonViewGoogle: {
+        width: "100%",
+        height: 55,
+        borderRadius: 25,
+        borderWidth: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        borderColor: colors.primary,
+        marginTop: 30,
+        flexDirection: "row",
+    },
+    loginButtonGoogle: {
+        color: colors.primary,
+        fontWeight: "bold",
+        fontSize: 17,
+    },
 
-  LoginButtonViewLogin: {
-    width: '90%',
-    height: 55,
-    borderRadius: 25,
-    borderWidth: 1,
-    backgroundColor: colors.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: 'white',
-    marginTop: 63,
-    flexDirection: 'row'
-  },
-  LoginButtonLogin: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 17,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
+    loginButtonViewFb: {
+        width: "100%",
+        height: 55,
+        borderRadius: 25,
+        borderWidth: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+        borderColor: colors.primary,
+        flexDirection: "row",
+    },
+    loginButtonFb: {
+        color: colors.primary,
+        fontWeight: "bold",
+        fontSize: 17,
+    },
+});
 
-
-  LoginButtonViewGoogle: {
-    width: '90%',
-    height: 55,
-    borderRadius: 25,
-    borderWidth: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: colors.primary,
-    marginTop: 30,
-    flexDirection: 'row'
-  },
-  LoginButtonGoogle: {
-    color: colors.primary,
-    fontWeight: 'bold',
-    fontSize: 17
-  },
-
-  LoginButtonViewFb: {
-    width: '90%',
-    height: 55,
-    borderRadius: 25,
-    borderWidth: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30,
-    borderColor: colors.primary,
-    flexDirection: 'row'
-  },
-  LoginButtonFb: {
-    color: colors.primary,
-    fontWeight: 'bold',
-    fontSize: 17
-  },
-
-
-})
-
-export default Login
+export default Login;
